fb3269e44a3bc7b6dfa5f41b7cc5ba65d2c6ef1a
gameegg <jongwon.baek@redduck.com>
2017-01-19 19:29:46 +0900

    change some consts

--- Assets/Scripts/Common/GameConsts.cs
+++ Assets/Scripts/Common/GameConsts.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public static class GameConsts
 {
     public const float shipCollisionRadius = 2;
-    public const float radarCollisionRadius = 14;
+    public const float radarCollisionRadius = 20;
 
     public const float preDelay = 2;
 
     public const int minShipCount = 1;
     public const int maxShipCount = 25;
     public const float maxShipHp = 5;
-    public const float maxShipSpd = 15;
+    public const float maxShipSpd = 30;
     public const float maxShipRotSpd = 360;
     public const float chargingMoveSlow = 0.3f;
     public const float chargingRotSlow = 0.5f;
@@ -21,7 +21,7 @@ public static class GameConsts
     public const float thiknessPerPower = 0.15f;
     public const float maxChargingPower = 5;
 
-    public const float basicGroundSize = 50;
+    public const float basicGroundSize = 100;
     public const float basicGroundTimer = 10;
     public const float groundSizeMultiplyer = 0.2f;
 
