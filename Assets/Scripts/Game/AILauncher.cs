4aa83defcb1b3b06f7795785428a8e6e61b5c9ea
gameegg <jongwon.baek@redduck.com>
2017-01-20 13:44:50 +0900

    random function

--- Assets/Scripts/Game/AILauncher.cs
+++ Assets/Scripts/Game/AILauncher.cs
@@ -18,6 +18,9 @@ public class AILauncher : MonoBehaviour {
     List<ShipJSObject> allEnemyShips = new List<ShipJSObject>();
     List<MyShipJSObject> allMyShips = new List<MyShipJSObject>();
 
+    int randomI = 0;
+    double[] randoms = {0.67875,0.32222,0.65738,0.38630,0.46512,0.96310,0.73474,0.68538,0.56789,0.92037,0.86292,0.30085,0.72147,0.19404,0.30749,0.23272,0.45703,0.51853,0.80288,0.21540,0.24340,0.42498,0.61466,0.51448,0.08059,0.38226,0.74283,0.12995,0.49975,0.39035,0.18740,0.79624,0.96973,0.98041,0.94837,0.04450,0.75611,0.62129,0.76015,0.34358,0.59329,0.57857,0.88833,0.95905,0.01246,0.85224,0.33290,0.62534,0.48244,0.17672,0.82828,0.87360,0.26881,0.81760,0.90564,0.17268,0.37158,0.77488,0.03382,0.78815,0.52516,0.04855,0.47839,0.45444,0.99514,0.63861,0.97378,0.70270,0.78151,0.27949,0.78556,0.00178,0.88428,0.23677,0.17931,0.61725,0.03787,0.51043,0.35831,0.28354,0.50784,0.83492,0.11927,0.53180,0.29422,0.47580,0.93105,0.83087,0.39699,0.43307,0.05923,0.44635,0.55057,0.70674,0.50380,0.11263,0.28613,0.29681,0.26476,0.36090};
+
     public void Init(Fleet fleet)
     {
         _fleet = fleet;
@@ -61,6 +64,7 @@ public class AILauncher : MonoBehaviour {
         engine.SetGlobalFunction("d2r", new Func<double, double>(D2R));
         engine.SetGlobalFunction("r2d", new Func<double, double>(R2D));
         engine.SetGlobalFunction("dist", new Func<ObjectInstance, ObjectInstance, double>(Distance));
+        engine.SetGlobalFunction("random", new Func<double>(Random));
     }
 
     /// <summary>
@@ -146,6 +150,9 @@ public class AILauncher : MonoBehaviour {
     {
         GameEvents.onLog.Invoke(_fleet.script,str);
         Debug.Log("JS LOG : " + str);
+    }
+    public double Random(){
+        return randoms[randomI++%randoms.Length];
     }
     public double Cos(double v)
     {
